27b262d99a5332074567da1e4b664edcaa13c03a
atomheartdani <contact@danielemeregalli.com>
2022-11-21 20:31:46 +0100

    Remove unused method

--- php/datamodel/user.php
+++ php/datamodel/user.php
@@ -10,24 +10,17 @@ class User
 	public $lastname;
 	public $email;
 	public $isAdmin;
+	public $password;
 
 	public function __construct($db)
 	{
 		$this->conn = $db;
 	}
 
-	function getAll()
-	{
-		$query = "SELECT a.* FROM users a ";
-		$stmt = $this->conn->prepare($query);
-		$stmt->execute();
-		return $stmt;
-	}
-
 	function getAllById($ids)
 	{
-    $in = str_repeat('?,', count($ids) - 1) . '?';
-		$query = "SELECT a.* FROM users a WHERE a.id IN ($in)";
+		$in = str_repeat('?,', count($ids) - 1) . '?';
+		$query = "SELECT u.* FROM users u WHERE u.id IN ($in)";
 		$stmt = $this->conn->prepare($query);
 		$stmt->execute($ids);
 		return $stmt;
