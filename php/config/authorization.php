63338049b9a66e89f59d3148e54aa3661a47ab63
atomheartdani <contact@danielemeregalli.com>
2023-01-07 11:39:57 +0100

    Ensure 401 error is returned to front end

--- php/config/authorization.php
+++ php/config/authorization.php
@@ -15,25 +15,30 @@ function checkAuthorization() {
   require 'include/config.php';
   require '../vendor/autoload.php';
 
-  // Check if Authorization header is present
-  if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
-    http_response_code(401);
-    die;
-  }
+  try {
+    // Check if Authorization header is present
+    if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+      http_response_code(401);
+      die;
+    }
 
-  // Check if Authorization header contains data
-  $token = $matches[1];
-  if (!$token) {
-    http_response_code(401);
-    die;
-  }
-  $decoded = JWT::decode($token, new Key($jwt_key, 'HS256'));
+    // Check if Authorization header contains data
+    $token = $matches[1];
+    if (!$token) {
+      http_response_code(401);
+      die;
+    }
+    $decoded = JWT::decode($token, new Key($jwt_key, 'HS256'));
 
-  $now = new DateTimeImmutable();
-  $serverName = 'climbers-soul-caiarosio-backend';
+    $now = new DateTimeImmutable();
+    $serverName = 'climbers-soul-caiarosio-backend';
 
-  // Check if jwt token is valid
-  if ($decoded->iss !== $serverName || $decoded->nbf > $now->getTimestamp() || $decoded->exp < $now->getTimestamp()) {
+    // Check if jwt token is valid
+    if ($decoded->iss !== $serverName || $decoded->nbf > $now->getTimestamp() || $decoded->exp < $now->getTimestamp()) {
+      http_response_code(401);
+      die;
+    }
+  } catch (Exception $e) {
     http_response_code(401);
     die;
   }
