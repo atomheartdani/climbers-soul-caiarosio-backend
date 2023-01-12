1ab60b0a538f8865ed093a84780f7bb378ffd659
atomheartdani <contact@danielemeregalli.com>
2023-01-12 20:40:52 +0100

    Allow cache headers

--- php/users/updatePassword.php
+++ php/users/updatePassword.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, X-Requested-With, Authorization');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, X-Requested-With, Authorization, Pragma, Cache-Control, Expires');
 header('Content-Type: application/json; charset=UTF-8');
 
 require '../config/authorization.php';
