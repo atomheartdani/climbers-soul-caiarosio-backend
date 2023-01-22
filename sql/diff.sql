81919c94027aa5c9e4aae75759cd53ff605003fd
atomheartdani <contact@danielemeregalli.com>
2023-01-22 14:46:53 +0100

    Remove diff file

--- sql/diff.sql
+++
@@ -1,7 +0,0 @@
-/*
-This is a temporary file to store sql modification until a next release
-When a new release is pushed, this file should be emptied and the modifications should be consolidated into the proper file
-*/
-
--- Add new tosConsent field to ClimbersSouluser
-ALTER TABLE `ClimbersSoulUsers` ADD COLUMN `tosConsent` tinyint(1) NOT NULL DEFAULT '0' AFTER `email`;
