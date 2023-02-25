24cb88eb0f05fec87cc01661cf38621bf4ef68ff
atomheartdani <contact@danielemeregalli.com>
2023-02-25 20:35:27 +0100

    Add endpoint for self-registering users Fix missing isVerified parameter on insert call

--- php/datamodel/user.php
+++ php/datamodel/user.php
@@ -57,8 +57,16 @@ class User {
 		return $stmt;
 	}
 
-	function insert($username, $firstname, $lastname, $email, $caiSection, $tosConsent, $defaultPassword, $canManageOpenings, $canManageUsers) {
-		$query = 'INSERT INTO ClimbersSoulUsers(`username`, `firstname`, `lastname`, `email`, `caiSection`, `tosConsent`, `updatePassword`, `password`, `canManageOpenings`, `canManageUsers`) VALUES (:username, :firstname, :lastname, :email, :caiSection, :tosConsent, 1, :defaultPassword, :canManageOpenings, :canManageUsers)';
+	function create($username, $firstname, $lastname, $email, $caiSection, $tosConsent, $defaultPassword, $canManageOpenings, $canManageUsers, $isVerified) {
+		$this->insert($username, $firstname, $lastname, $email, $caiSection, $tosConsent, 1, $defaultPassword, $canManageOpenings, $canManageUsers, $isVerified);
+	}
+
+	function register($username, $firstname, $lastname, $email, $caiSection, $password) {
+		$this->insert($username, $firstname, $lastname, $email, $caiSection, 0, 0, $password, 0, 0, 0);
+	}
+
+	private function insert($username, $firstname, $lastname, $email, $caiSection, $tosConsent, $updatePassword, $defaultPassword, $canManageOpenings, $canManageUsers, $isVerified) {
+		$query = 'INSERT INTO ClimbersSoulUsers(`username`, `firstname`, `lastname`, `email`, `caiSection`, `tosConsent`, `updatePassword`, `password`, `canManageOpenings`, `canManageUsers`, `isVerified`) VALUES (:username, :firstname, :lastname, :email, :caiSection, :tosConsent, :updatePassword, :defaultPassword, :canManageOpenings, :canManageUsers, :isVerified)';
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(':username', $username);
 		$stmt->bindParam(':firstname', $firstname);
@@ -66,9 +74,11 @@ class User {
 		$stmt->bindParam(':email', $email);
 		$stmt->bindParam(':caiSection', $caiSection);
 		$stmt->bindParam(':tosConsent', $tosConsent);
+		$stmt->bindParam(':updatePassword', $updatePassword);
 		$stmt->bindParam(':defaultPassword', $defaultPassword);
 		$stmt->bindParam(':canManageOpenings', $canManageOpenings);
 		$stmt->bindParam(':canManageUsers', $canManageUsers);
+		$stmt->bindParam(':isVerified', $isVerified);
 		$stmt->execute();
 	}
 
--- php/users/register.php
+++ php/users/register.php
@@ -10,23 +10,17 @@ require '../config/database.php';
 require '../datamodel/user.php';
 
 managePreflight();
-checkAuthorization();
 
 $postData = file_get_contents('php://input');
 
 if (isset($postData) && !empty($postData)) {
   $parsedData = json_decode($postData, true);
-  $id = $parsedData['id'];
   $username = $parsedData['username'];
   $firstname = $parsedData['firstname'];
   $lastname = $parsedData['lastname'];
   $email = $parsedData['email'];
   $caiSection = $parsedData['caiSection'];
-  $tosConsent = intval($parsedData['tosConsent']);
-  $updatePassword = intval($parsedData['updatePassword']);
-  $canManageOpenings = intval($parsedData['canManageOpenings']);
-  $canManageUsers = intval($parsedData['canManageUsers']);
-  $isVerified = intval($parsedData['isVerified']);
+  $password = $parsedData['password'];
 } else {
   http_response_code(400);
   die;
@@ -38,16 +32,9 @@ try {
 
   $user = new User($db);
 
-  if ($id == 0) {
-    $defaultPassword = createNewPassword($username);
-    $user->create($username, $firstname, $lastname, $email, $caiSection, $tosConsent, $defaultPassword, $canManageOpenings, $canManageUsers, $isVerified);
-  } else {
-    $user->update($id, $username, $firstname, $lastname, $email, $caiSection, $tosConsent, $canManageOpenings, $canManageUsers, $isVerified);
-    if($updatePassword == 1) {
-      $defaultPassword = createNewPassword($username);
-      $user->updatePassword($id, $defaultPassword, 1);
-    }
-  }
+  $securedPassword = createNewPassword($password);
+  $user->register($username, $firstname, $lastname, $email, $caiSection, $securedPassword);
+
   http_response_code(200);
 } catch (Exception $e) {
   error_log('Error in users/save: ' . $e, 0);
